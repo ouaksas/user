9c14cbc293a3d10bed3eac77576017a5572a0eaa
Nohayla Zaoui <omarouaksas98@gmail.com>
2019-05-20 04:15:42 +0200

    rep user

---
+++ users.js
@@ -0,0 +1,26 @@
+let users = [{
+        name: 'user1',
+        bio: 'bio biob bkldjha',
+        img_url: 'https://via.placeholder.com/500.png?text=user1'
+    },
+    {
+        name: 'user2',
+        bio: 'bio biob bkldjha',
+        img_url: 'https://via.placeholder.com/500.png?text=user2'
+    },
+    {
+        name: 'user3',
+        bio: 'bio biob bkldjha',
+        img_url: 'https://via.placeholder.com/500.png?text=user3'
+    },
+    {
+        name: 'user4',
+        bio: 'bio biob bkldjha',
+        img_url: 'https://via.placeholder.com/500.png?text=user4'
+    },
+    {
+        name: 'user5',
+        bio: 'bio biob bkldjha',
+        img_url: 'https://via.placeholder.com/500.png?text=user5'
+    }
+];
